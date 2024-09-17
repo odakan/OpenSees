d96266bb192ee1382ae9db0fc14a6050803c39e7
Onur Deniz AKAN <onurdenizakan@gmail.com>
2024-09-17 18:36:31 +0200

    fix-missing-end-of-line

--- SRC/interpreter/OpenSeesNDMaterialCommands.cpp
+++ SRC/interpreter/OpenSeesNDMaterialCommands.cpp
@@ -169,8 +169,8 @@ namespace {
 	nDMaterialsMap.insert(std::make_pair("MultiYieldSurfaceClay", &OPS_MultiYieldSurfaceClay));
 	nDMaterialsMap.insert(std::make_pair("PressureDependMultiYield", &OPS_PressureDependMultiYield));
 	nDMaterialsMap.insert(std::make_pair("PressureDependMultiYield02", &OPS_PressureDependMultiYield02));
-	nDMaterialsMap.insert(std::make_pair("PIMYImplex", &OPS_PIMYImplex))
-	nDMaterialsMap.insert(std::make_pair("PDMY02Implex", &OPS_PDMY02Implex))
+	nDMaterialsMap.insert(std::make_pair("PIMYImplex", &OPS_PIMYImplex));
+	nDMaterialsMap.insert(std::make_pair("PDMY02Implex", &OPS_PDMY02Implex));
 	nDMaterialsMap.insert(std::make_pair("FluidSolidPorous", &OPS_FluidSolidPorousMaterial));
 	nDMaterialsMap.insert(std::make_pair("PlaneStressMaterial", &OPS_PlaneStress));
 	nDMaterialsMap.insert(std::make_pair("PlaneStress", &OPS_PlaneStress));
